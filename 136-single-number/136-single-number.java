e56f28a4f0b695a501da9eb4530f94d4c3637065
Bhishek29 <77242292+Bhishek29@users.noreply.github.com>
2022-09-01 01:59:26 +0530

    Time: 868 ms (5.00%), Space: 50.7 MB (48.26%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,16 +1,19 @@
 class Solution {
     public int singleNumber(int[] nums) {
         
+        //brute force approach ;
         
+        //if length of array is 1 we return the only element as ans l
         if(nums.length == 1){
             return nums[0];
         }
         
+        //else what we do is we use 2 for loop and keep checking if it comes twice or not
+        //if not then we return the element ;
         for(int i = 0 ; i < nums.length ; i++){
-            int temp = nums[i];
             int count = 0 ;
             for(int j = 0 ; j < nums.length ;j++){
-                if(nums[j]==temp){
+                if(nums[j]==nums[i]){
                     count++;
                 }
             }
