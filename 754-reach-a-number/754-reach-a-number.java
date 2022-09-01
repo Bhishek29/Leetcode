0a15de04f7da52b3721365d589205b6879f9d461
Bhishek29 <77242292+Bhishek29@users.noreply.github.com>
2022-09-02 01:16:47 +0530

    Time: 6 ms (20.06%), Space: 41.7 MB (6.79%) - LeetHub

---
+++ 754-reach-a-number/754-reach-a-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int reachNumber(int target) {
+       
+        int steps = 1 ; int sum = 0 ;
+        
+        if (target < 0 ){
+            target = 0 - target ;
+        }
+        
+        if(target == 0){
+            return 0;
+        }
+        
+        while(sum < target){
+            sum = sum + steps;
+            steps++;
+        }
+        
+        while((target-sum) % 2 != 0 ){
+            sum = sum + steps ;
+            steps++;
+        }
+        
+        return steps - 1;
+        
+    }
+}
